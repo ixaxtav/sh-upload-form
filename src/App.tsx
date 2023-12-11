1a42be469823eabe5d46a2255bbb0036fe418352
Ixax Tavarez <ixaxtav@gmail.com>
2023-12-11 03:58:45 -0500

    Create layouts folder and add center container

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { Modal, Button } from "./components";
 import { MdClose } from "react-icons/md";
+import { CenterContainer } from "./layouts/CenterContainer";
 
 function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div>
+    <CenterContainer>
       <Button onClick={() => setIsOpen(true)}>Document Upload</Button>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <Button onClick={() => setIsOpen(false)}>
           <MdClose />
         </Button>
       </Modal>
-    </div>
+    </CenterContainer>
   );
 }
 
---
+++ src/layouts/CenterContainer.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CenterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  height: 100vh;
+`;
